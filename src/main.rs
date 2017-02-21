2aee95d8e11b734e5a4d4dfca9a478588acb7911
Eric Slenk <slenkeri@anr.msu.edu>
2017-02-21 17:14:11 -0500

    Implement dummy main.

--- src/main.rs
+++ src/main.rs
@@ -80,7 +80,21 @@ impl std::fmt::Display for World {
 
 fn main() {
     // TODO: Command line interface
-    println!("Hello, world!");
+    println!("Dummy world test.");
+
+    let mut world = World {
+        cells: [[0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0]],
+    };
+
+    for i in 0..10 {
+        println!("== Generation {} ==", i);
+        println!("{}", world);
+        world = world.evolve();
+    }
 }
 
 
