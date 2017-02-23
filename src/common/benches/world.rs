058396130bc41918f0fccacce8cd1630747abecc
Eric Slenk <slenkeri@anr.msu.edu>
2017-02-23 16:56:00 -0500

    Create benchmark for World::evolve().

--- src/common/benches/world.rs
+++ src/common/benches/world.rs
@@ -6,10 +6,20 @@ use bencher::Bencher;
 use common::world::World;
 
 fn bench_new(b: &mut Bencher) {
-    b.iter(||{
-        World::new();
-    });
+    b.iter(|| { World::new(); });
 }
 
-benchmark_group!(benches, bench_new);
+fn bench_evolve(b: &mut Bencher) {
+    let world = World {
+        cells: [[0, 0, 0, 0, 0],
+                [0, 1, 1, 0, 0],
+                [0, 1, 0, 1, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0]],
+    };
+
+    b.iter(|| { world.evolve(); })
+}
+
+benchmark_group!(benches, bench_new, bench_evolve);
 benchmark_main!(benches);
